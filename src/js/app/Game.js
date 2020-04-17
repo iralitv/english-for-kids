01651ce97080c13ba8648eaa9dce0bda3be921e1
Iralitv <airyne.litvinchuk@gmail.com>
2020-04-17 13:51:59 +0300

    fix: eslint errors are fixed

--- src/js/app/Game.js
+++ src/js/app/Game.js
@@ -1,8 +1,8 @@
-import { buildCard } from './cardTemplate';
-import cards from "../../../data/cards";
-import builtHtmlElement from "./templateHelper";
-import {isMenuCategory, isTrainMode} from "./flags";
-import {playAudio, rotateCard, shuffleArray} from "./heplers";
+import buildCard from './cardTemplate';
+import cards from '../../../data/cards';
+import builtHtmlElement from './templateHelper';
+import { isMenuCategory, isTrainMode } from './flags';
+import { playAudio, rotateCard, shuffleArray } from './heplers';
 
 class Game {
   constructor() {
@@ -67,9 +67,11 @@ class Game {
     this.elements.main.appendChild(this.elements.cardContainer);
     this.elements.main.appendChild(this.elements.startButton);
 
-    if(!isTrainMode(localStorage.getItem('mode'))) {
-      this.elements.cardContainer.childNodes.forEach(item => item.classList.add('card--play'));
-      (!isMenuCategory) && this.elements.startButton.classList.remove('hidden');
+    if (!isTrainMode(localStorage.getItem('mode'))) {
+      this.elements.cardContainer.childNodes.forEach((item) => item.classList.add('card--play'));
+      if (!isMenuCategory) {
+        this.elements.startButton.classList.remove('hidden');
+      }
     }
 
     document.querySelector('.switch-field').addEventListener('click',
@@ -83,13 +85,13 @@ class Game {
     const fragment = document.createDocumentFragment();
     let categoryIndex = 0;
     cards[0].forEach((item, index) => {
-      if(item.name === this.props.category) {
+      if (item.name === this.props.category) {
         categoryIndex = index;
       }
     });
 
     const dataCards = cards[categoryIndex];
-    dataCards.forEach(item => {
+    dataCards.forEach((item) => {
       const card = buildCard(item, this.props.mode);
       this.data.audio.push(item.audioSrc);
       fragment.appendChild(card);
@@ -102,18 +104,16 @@ class Game {
     const currentChild = event.target.closest('.card__face') && event.target.closest('.card__face').parentNode;
     this.props.currentCardIndex = [...event.currentTarget.childNodes].indexOf(currentChild);
 
-    if(this.props.currentCardIndex !== -1) {
-      if (isTrainMode(localStorage.getItem('mode'))){
-
-        (!event.target.classList.contains('card__desc--button'))
-          ? playAudio(`data/${this.data.audio[this.props.currentCardIndex]}`)
-          : rotateCard(event.currentTarget.children[this.props.currentCardIndex]);
-
+    if (this.props.currentCardIndex !== -1) {
+      if (isTrainMode(localStorage.getItem('mode'))) {
+        if (!event.target.classList.contains('card__desc--button')) {
+          playAudio(`data/${this.data.audio[this.props.currentCardIndex]}`);
+        } else {
+          rotateCard(event.currentTarget.children[this.props.currentCardIndex]);
+        }
       } else if (!isTrainMode(localStorage.getItem('mode')) && this.game.isStart) {
-
         if (this.game.currentAudioIndex !== this.props.currentCardIndex) {
-
-          if(currentChild.classList.contains('blur')) {
+          if (currentChild.classList.contains('blur')) {
             event.stopPropagation();
           } else {
             playAudio('data/audio/error.mp3');
@@ -121,39 +121,30 @@ class Game {
             const cloneStar = this.elements.starError.cloneNode(true);
             this.elements.stars.append(cloneStar);
           }
+        } else if (this.game.shuffleAudio.length) {
+          const [shuffleItem] = this.game.shuffleAudio.splice(0, 1);
+          this.game.currentAudio = shuffleItem;
+          this.game.currentAudioIndex = this.data.audio.indexOf(this.game.currentAudio);
 
-        } else {
-
-          if(this.game.shuffleAudio.length) {
-            this.game.currentAudio = this.game.shuffleAudio.splice(0, 1)[0];
-            this.game.currentAudioIndex = this.data.audio.indexOf(this.game.currentAudio);
-
-            if(currentChild.classList.contains('blur')) {
-              event.stopPropagation();
-            } else {
-              this.game.guessArray.push('cool');
-              const cloneStar = this.elements.starCorrect.cloneNode(true);
-              this.elements.stars.append(cloneStar);
-              playAudio('data/audio/correct.mp3');
-              currentChild.classList.add('blur');
-            }
-
-            setTimeout(() => playAudio(`data/${this.game.currentAudio}`), 500);
+          if (currentChild.classList.contains('blur')) {
+            event.stopPropagation();
           } else {
-            this.checkResult(this.game.guessArray);
-            this.game.isStart = false;
-            this.game.guessArray.length = 0;
-            this.elements.startButton.classList.remove('button--repeat');
-            this.elements.cardContainer.childNodes.forEach(item => item.classList.remove('blur'));
-            this.elements.stars.innerHTML = '';
+            this.game.guessArray.push('cool');
+            const cloneStar = this.elements.starCorrect.cloneNode(true);
+            this.elements.stars.append(cloneStar);
+            playAudio('data/audio/correct.mp3');
+            currentChild.classList.add('blur');
           }
 
+          setTimeout(() => playAudio(`data/${this.game.currentAudio}`), 500);
+        } else {
+          this.checkResult(this.game.guessArray);
+          this.game.isStart = false;
+          this.game.guessArray.length = 0;
+          this.elements.startButton.classList.remove('button--repeat');
+          this.elements.cardContainer.childNodes.forEach((item) => item.classList.remove('blur'));
+          this.elements.stars.innerHTML = '';
         }
-        //
-        // console.log(this.game.shuffleAudio);
-        // console.log('audio: ', this.game.currentAudioIndex);
-        // console.log('card: ', this.props.currentCardIndex)
-        // console.log('guess: ', this.game.guessArray)
       }
     }
   }
@@ -161,22 +152,25 @@ class Game {
   changeMode(event) {
     localStorage.setItem('mode', event.target.value);
 
-    if(isTrainMode(event.target.value)) {
-      this.elements.cardContainer.childNodes.forEach(item => item.classList.remove('card--play'));
+    if (isTrainMode(event.target.value)) {
+      this.elements.cardContainer.childNodes.forEach((item) => item.classList.remove('card--play'));
       this.elements.startButton.classList.add('hidden');
     }
 
-    if(!isTrainMode(event.target.value)){
-      this.elements.cardContainer.childNodes.forEach(item => item.classList.add('card--play'));
-      (!isMenuCategory) && this.elements.startButton.classList.remove('hidden');
+    if (!isTrainMode(event.target.value)) {
+      this.elements.cardContainer.childNodes.forEach((item) => item.classList.add('card--play'));
+      if (!isMenuCategory) {
+        this.elements.startButton.classList.remove('hidden');
+      }
     }
   }
 
   startGame(event) {
     this.game.isStart = true;
-    if(!event.target.classList.contains('button--repeat')) {
+    if (!event.target.classList.contains('button--repeat')) {
       this.game.shuffleAudio = shuffleArray(this.data.audio);
-      this.game.currentAudio = this.game.shuffleAudio.splice(0, 1)[0];
+      const [shuffleItem] = this.game.shuffleAudio.splice(0, 1);
+      this.game.currentAudio = shuffleItem;
       this.game.currentAudioIndex = this.data.audio.indexOf(this.game.currentAudio);
     }
     event.currentTarget.classList.add('button--repeat');
@@ -185,29 +179,28 @@ class Game {
 
   checkResult(array) {
     const error = array
-      .map(item => item === 'error')
+      .map((item) => item === 'error')
       .filter(Boolean)
       .length;
 
     const resultText = this.elements.modal.querySelector('.result__text');
     const resultImage = this.elements.modal.querySelector('.result__img');
     this.elements.modal.addEventListener('click', (event) => {
-      if(event.target.classList.contains('modal__close') || event.target === this.elements.modal) {
+      if (event.target.classList.contains('modal__close') || event.target === this.elements.modal) {
         this.elements.modal.classList.remove('visible');
       }
     });
 
-    if(!error) {
+    if (!error) {
       playAudio('data/audio/success.mp3');
       resultText.innerText = 'You are COOL! Without errors';
-      resultImage.setAttribute('src', 'data/img/success.jpg')
+      resultImage.setAttribute('src', 'data/img/success.jpg');
     } else {
       playAudio('data/audio/failure.mp3');
-      resultText.innerText =
-        (error === 1)
-          ? `Almost! With ${error} error`
-          : `You are lalka! With ${error} errors` ;
-      resultImage.setAttribute('src', 'data/img/failure.jpg')
+      resultText.innerText = (error === 1)
+        ? `Almost! With ${error} error`
+        : `You are lalka! With ${error} errors`;
+      resultImage.setAttribute('src', 'data/img/failure.jpg');
     }
     this.elements.modal.classList.add('visible');
   }
--- src/js/app/heplers.js
+++ src/js/app/heplers.js
@@ -1,12 +1,12 @@
 function rotateCard(item) {
-  item.classList.toggle('is-flipped')
+  item.classList.toggle('is-flipped');
 }
 
 const playAudio = (url) => new Audio(url).play();
 
 const shuffleArray = (array) => array
-    .map((a) => [Math.random(), a])
-    .sort((a, b) => a[0] - b[0])
-    .map((a) => a[1]);
+  .map((a) => [Math.random(), a])
+  .sort((a, b) => a[0] - b[0])
+  .map((a) => a[1]);
 
-export { rotateCard, playAudio, shuffleArray }
+export { rotateCard, playAudio, shuffleArray };
